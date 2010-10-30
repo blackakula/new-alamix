be0942c1f0c1d1e86f54d3c5eb07e5f061285784
Black Akula <blackakula@gmail.com>
2010-10-30 20:00:51 +0300

    Finished index. Test with right column images

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,6 +3,18 @@
     public function index() {
       $this->set('title','This is main page');
       $this->set('stylesheets', array('index.css'));
+      
+      $content_items = array();
+      foreach (array('news','media','portfolio') as $v) {
+      	$content_items[$v] = array('title' => 'Заголовок новини', 'style' => $v, 'date' => '30/X', 'text' => 'Текст новини. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis venenatis sollicitudin. Sed ac risus metus, et viverra tortor.');
+      }
+      $content_items['news']['category'] = 'новини';
+      $content_items['news']['icons'] = array('ico-media','ico-portfolio','ico-contacts');
+      $content_items['media']['category'] = 'медіа';
+      $content_items['media']['icons'] = array('ico-news','ico-contacts','ico-portfolio');
+      $content_items['portfolio']['category'] = 'портфоліо';
+      $content_items['portfolio']['icons'] = array('ico-contacts','ico-media','ico-news');
+      $this->set('content_items', $content_items);
     }
   }
 ?>
