bfef69ae9918f94084e191c9ee50b4a45171ed5a
Black Akula <blackakula@gmail.com>
2010-11-28 20:40:48 +0200

    Gallery added. Page finished

--- app/controllers/PhotoController.php
+++ app/controllers/PhotoController.php
@@ -2,10 +2,26 @@
   class PhotoController extends ApplicationController {
     public function index() {
       $this->set('stylesheets', array('photo.css'));
+      $this->set('javascripts', array(
+            'jquery-1.4.4.min.js',
+            'gallery/eye.js',
+            'gallery/utils.js',
+            'gallery/spacegallery.js',
+            'photo.js'
+          ));
       $data = get_config('photo');
       $this->set('title',$data['title']);
       $this->set('h1_title', $data['h1']);
       $this->set('content_items', $data['content']);
+
+      $img_path = 'gallery';
+      $gallery_dir = opendir(get_config('ROOT_DIR').'/public/img/'.$img_path);
+      $photos = array();
+      while ($photo = readdir($gallery_dir)) {
+        if ($photo == '.' || $photo == '..') continue;
+        $photos[] = $img_path.'/'.$photo;
+      }
+      $this->set('gallery_photos', $photos);
     }
   }
 ?>
--- lib/my/ApplicationController.php
+++ lib/my/ApplicationController.php
@@ -4,6 +4,7 @@
       parent::__construct($params);
       /* Here is your code to do for all controllers */
       $this->header()->charset();
+      $this->set('javascripts', array());
     }
   }
 ?>
